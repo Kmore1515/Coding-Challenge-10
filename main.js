66993eed447a5f8e64026ff1cb52a99a09e37c26
Kmore1515 <kylebradleymorris@usf.edu>
2024-10-20 23:50:08 -0400

    Final

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log("Product 1");
+let sizeSelector = document.getElementById("size-selector");
+let priceElement = document.getElementById("Product Price");
+let purchaseItemButton = document.getElementById("purchase-item")
+
+sizeSelector.addEventListener("change", (event) => {
+    let selectedPrice = event.target.value
+    priceElement.textContent = `$${selectedPrice}`
+})  
+ function disableButton() {
+    if(stock === "Out of Stock") {
+        purchaseItemButton.disable = true;
+    }
+    else
+        purchaseItemButton.disable = false;
+ }
+ function checkoutEvent() {
+    if(stock === "In Stock") {
+        alert(`Your purchase has been completed!`)
+    }
+    else alert(`Order could not be fullfilled due to lack of stock.`)
+ }
+
